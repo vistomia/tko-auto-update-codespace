71af06cd482d0d22c2386d4e7705006b510c5b71
Victor Farias <114350682+vistomia@users.noreply.github.com>
2024-12-14 16:50:09 +0000

    teste

---
+++ .devcontainer/attach.sh
@@ -0,0 +1,19 @@
+set +x 
+pipx upgrade tko && clear
+echo Exemplos de mensagens:
+echo -e "\033[1;32mTudo pronto para mais uma jornada! 😊\033[0m"
+echo -e "\033[1;32mProva Turma PDD Quinta-feira \033[0m"
+echo -e "\033[1;32mProva Turma PDD Quarta-feira  31/02/2025\033[0m"
+echo -e "\033[1;32mBoa teste! \033[0m"
+echo -e "\033[1;32mBoa prova! \033[0m"
+tko -v
+echo ""
+echo ""
+read -p "[pressione a tecla Enter para continuar]"
+
+if [ -d poo ]; then
+    tko play poo
+else
+    echo s | tko init --remote poo
+    echo ts | tko play poo
+fi
